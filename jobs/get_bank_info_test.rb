5b907bfa28ee9cdd7734812cd8362f44e9cffe95
rick <tcvrick@gmail.com>
2016-02-20 14:45:34 -0500

    Added bank balance to main

--- jobs/get_bank_info_test.rb
+++ jobs/get_bank_info_test.rb
@@ -2,7 +2,7 @@ require "net/http"
 require "uri"
 require "json"
 
-SCHEDULER.every '60s' do
+SCHEDULER.every '10s' do
 
   url = URI.parse('https://tartan.plaid.com/connect')
   req = Net::HTTP::Post.new(url.request_uri)
@@ -10,12 +10,14 @@ SCHEDULER.every '60s' do
   http = Net::HTTP.new(url.host, url.port)
   http.use_ssl = (url.scheme == "https")
   response = http.request(req)
-  # puts response.body
   json = JSON.parse(response.body)
-  json["accounts"].each do |account|
-    # puts account
-    # puts "Account ID:" + account["_id"]
-    puts account["meta"]["name"]
-    puts "Account Balance: $" + String(account["balance"]["available"])
-  end
+  # json["accounts"].each do |account|
+  #   puts account["meta"]["name"]
+  #   puts "Account Balance: $" + String(account["balance"]["available"])
+  # end
+
+  account_name = json["accounts"].at(0)["meta"]["name"] + " Account"
+  account_balance = json["accounts"].at(0)["balance"]["available"]
+
+  send_event("bank-balance", title: account_name, current: account_balance)
 end
